72a4950da680f99aa53bb97357a3cfc0b40b16bd
GustavOnsberg <gustavonsberg@gmail.com>
2022-01-23 05:34:20 +0100

    266 multi threading WIP

--- problem_0266.py
+++ problem_0266.py
@@ -1,7 +1,5 @@
 from math import sqrt
 
-import numpy as np
-
 from helper.prime_numbers import *
 from time import time
 import threading
@@ -15,32 +13,34 @@ start_time = 0
 sq = 0
 psr = 0
 
-seg_size = 1
+seg_size = 20
+segs_done = 0
+total_segs = 0
 
 end_get_base_url = False
 
 
 def get_base_mul():
     global end_get_base_url
-    if end_get_base_url:
-        return -1
-
     global bin_array_0
 
-    bin_array_0[0] = not bin_array_0[0]
-    incr = not bin_array_0[0]
+    if end_get_base_url:
+        return -1
 
     if bin_array_0[0]:
         num = prime_array_0[0]
     else:
         num = 1
 
+    incr = bin_array_0[0]
+    bin_array_0[0] = not bin_array_0[0]
+
     for x in range(1, len(prime_array_0)):
-        if not bin_array_0[x - 1] and incr:
-            bin_array_0[x] = not bin_array_0[x]
-            incr = not bin_array_0[x]
         if bin_array_0[x]:
             num *= prime_array_0[x]
+        if not bin_array_0[x - 1] and incr:
+            incr = bin_array_0[x]
+            bin_array_0[x] = not bin_array_0[x]
 
     if True not in bin_array_0:
         end_get_base_url = True
@@ -48,49 +48,63 @@ def get_base_mul():
     return num
 
 
-def test_seg():
+def test_seg(thread_num):
     global psr
+    global segs_done
 
     running = True
 
     while running:
+        base_mul = get_base_mul()
         bin_array = [False] * seg_size
         for _ in range(2 ** seg_size):
-            bin_array[0] = not bin_array[0]
-            incr = not bin_array[0]
 
-            num = get_base_mul()
-
-            if num == -1:
-                print("seg done")
-                running = False
-                break
-
+            num = base_mul
             if bin_array[0]:
                 num *= prime_array_1[0]
 
-            for x in range(1, len(prime_array_1)):
+            bin_array[0] = not bin_array[0]
+            incr = not bin_array[0]
+
+            if num == -1:
+                print("seg done")
+                return
+                running = False
+                break
+
+            for x in range(1, seg_size):
+                if bin_array[x]:
+                    num *= prime_array_1[x]
                 if not bin_array[x - 1] and incr:
                     bin_array[x] = not bin_array[x]
                     incr = not bin_array[x]
-                if bin_array[x]:
-                    num *= prime_array_1[x]
 
             if psr < num <= sq:
                 psr = num
                 print(f"PSR updated: {psr}")
 
+        segs_done += 1
+        progress = segs_done / total_segs
+        time_s = int((1 - progress) / progress * (time() - start_time))
+        time_m = int(time_s / 60)
+        time_s = time_s % 60
+        time_h = int(time_m / 60)
+        time_m = time_m % 60
+        time_d = int(time_h / 24)
+        time_h = time_h % 24
+        print(f"{(progress * 100):.5f}% - {time_d}d {time_h:02d}d {time_m:02d}m {time_s:02d}s")
 
-def solve(n):
+
+def solve(n, t):
     global prime_array_0
     global bin_array_0
     global prime_array_1
     global bin_array_1
     global p
     global sq
+    global total_segs
 
     prime_array_0 = [2]
-    bin_array_0 = [False]
     p = 2
 
     i = next_prime(2)
@@ -109,13 +123,29 @@ def solve(n):
     print(f"{prime_array_0=} {prime_array_1=}")
 
     bin_array_0 = [False] * len(prime_array_0)
-    total_runs = 2 ** len(prime_array_0)
+    total_segs = 2 ** len(prime_array_0)
 
-    test_seg()
+    threads = []
+
+    for x in range(t):
+        threads.append(threading.Thread(target=test_seg, args=(x,)))
+        threads[x].start()
+        print(f"created thread: {x}")
+
+    for x in range(t):
+        threads[x].join()
 
     return 1
 
 
 if __name__ == "__main__":
     start_time = time()
-    print(solve(80))
+    solve(190, 16)
+    print(f"{psr=}")
+
+
+def run(t):
+    global start_time
+    start_time = time()
+    solve(190, t)
+    print(f"{psr=}")
